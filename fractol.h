8eadf798b4b2535306a47145f361dbdb9d219844
Mafalda Dias <138318938+JorjaKark@users.noreply.github.com>
2024-07-26 19:04:53 +0100

    Add files via upload

---
+++ fractol.h
@@ -0,0 +1,20 @@
+#ifndef FRACTOL_H
+#define FRACTOL_H
+
+#include <mlx.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+#define MAX_ITER 1000
+
+typedef struct s_complex
+{
+    double r;
+    double i;
+}               t_complex;
+
+#endif
+
+https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html
